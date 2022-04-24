74a47a2700fd6fe7965fb32a3c65b2e6199dd009
freemt <yucongo+fmt@gmail.com>
2022-04-24 15:30:15 +0800

    Update moer tests added

--- tests/test_set_loglevel.py
+++ tests/test_set_loglevel.py
@@ -1,5 +1,8 @@
 """Test set_loglevel."""
 # pylint: disable=broad-except
+# import importlib
+import os
+
 from set_loglevel import __version__
 from set_loglevel import set_loglevel
 
@@ -15,3 +18,47 @@ def test_sanity():
         assert not set_loglevel()
     except Exception:
         assert True
+
+
+# def test_force(capsys):
+# @pytest.fixture(autouse=True)
+def test_force(caplog):
+    """Test force=True."""
+    caplog.set_level(10)
+
+    os.environ["LOGLEVEL"] = "debug"
+    assert set_loglevel() == 10
+    assert set_loglevel(20, force=True) == 20
+
+    os.environ["LOGLEVEL"] = ""
+    assert set_loglevel() == 20
+    assert set_loglevel(10, force=True) == 10
+
+    _ = """
+    importlib.reload(logging)
+    logging.basicConfig(level=set_loglevel())
+    logging.getLogger().debug("boo %s", "arg")
+    logging.getLogger().info("info %s", "arg")
+
+    assert "boo" in caplog.record_tuples
+
+    out, err = capsys.readouterr()
+    # assert '0' in out
+    # assert '0' in err
+    sys.stderr.write(f"out: {out}")
+    sys.stderr.write(f"err: {err}")
+
+    logzero.loglevel(set_loglevel(20, force=True))
+    logger.debug(0)
+    out, err = capsys.readouterr()
+    # assert not out
+    # assert not err
+    sys.stderr.write(f"out: {out}")
+    sys.stderr.write(f"err: {err}")
+
+    print("print out")
+
+    logger.info(1)
+    out, err = capsys.readouterr()
+    assert "0" in err
+    """
